08cc7ab1b882154149a40d24edfd4671db88a47b
ika911 <64107958+ika911@users.noreply.github.com>
2020-04-28 13:38:50 -0700

    updated

--- src/Day_1/Methods.java
+++ src/Day_1/Methods.java
@@ -5,5 +5,6 @@ import javax.crypto.spec.PSource;
 public class Methods {
     public static void main(String[] args) {
         System.out.println("Methodssss");
+        System.out.println("BlueSprint is BadAss");
     }
 }
