eb7168d15cf72c29e6e16260d524be45c143b238
ika911 <64107958+ika911@users.noreply.github.com>
2020-04-28 13:03:18 -0700

    Update HappyBirthday.java

--- src/Day_1/HappyBirthday.java
+++ src/Day_1/HappyBirthday.java
@@ -3,5 +3,6 @@ package Day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday Batch18");
+        System.out.println("Happy Bday again ppl");
     }
 }
